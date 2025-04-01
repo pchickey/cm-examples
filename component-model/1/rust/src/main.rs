df1d1ee9060ad9a511ae937ed8239aa5c9522c1e
Pat Hickey <p.hickey@f5.com>
2025-03-31 17:11:15 -0700

    more

--- component-model/1/rust/src/main.rs
+++ component-model/1/rust/src/main.rs
@@ -1,13 +1,18 @@
+// Bindings generator creates a mod `exports` for the default world.
 wit_bindgen::generate!({ path: "../wit" });
 
+// Define some library code
+struct S;
+// And connect that library code to the single set of exports from this bin
 export!(S);
 
-struct S;
-
+// Implement the requirements for the exports
 impl exports::example::hello::say::Guest for S {
     fn hello(name: String) -> String {
         format!("Hello, {name}")
     }
 }
 
+// Ignore this, its so that `cargo build --target wasm32-wasip2` works without any messing around.
+// This will hopefully one day not be needed.
 fn main() {}
